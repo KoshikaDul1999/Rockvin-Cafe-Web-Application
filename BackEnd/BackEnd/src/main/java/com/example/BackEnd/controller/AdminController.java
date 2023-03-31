8fd9649bcd1275d33deeea5655e9a3e9d2d6303c
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-03-31 13:00:01 +0530

    create the view, update and delete user

--- BackEnd/BackEnd/src/main/java/com/example/BackEnd/controller/AdminController.java
+++ BackEnd/BackEnd/src/main/java/com/example/BackEnd/controller/AdminController.java
@@ -1,12 +1,12 @@
 package com.example.BackEnd.controller;
 
+import com.example.BackEnd.exception.AdminNotFoundException;
 import com.example.BackEnd.model.Admin;
 import com.example.BackEnd.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @CrossOrigin("http://localhost:3000")
@@ -20,5 +20,36 @@ public class AdminController {
         return adminRepository.save(newAdmin);
     }
 
+    @GetMapping("/admins")
+    List<Admin> getAllAdmins(){
+        return adminRepository.findAll();
+    }
+
+    @GetMapping("/admin/{id}")
+    Admin getAdminById(@PathVariable int id){
+        return adminRepository.findById(id)
+                .orElseThrow(()->new AdminNotFoundException(id));
+    }
+
+    @PutMapping("/admin/{id}")
+    Admin updateAdmin(@RequestBody Admin newAdmin,@PathVariable int id){
+        return adminRepository.findById(id)
+                .map(admin -> {
+                    admin.setAdmin_username(newAdmin.getAdmin_username());
+                    admin.setAdmin_name(newAdmin.getAdmin_name());
+                    admin.setAdmin_password(newAdmin.getAdmin_password());
+                    admin.setAdmin_email(newAdmin.getAdmin_email());
+                    return adminRepository.save(admin);
+                }).orElseThrow(()->new AdminNotFoundException(id));
+    }
+
+    @DeleteMapping("admin/{id}")
+    String deleteAdmin(@PathVariable int id){
+        if(!adminRepository.existsById(id)){
+            throw new AdminNotFoundException(id);
+        }
+        adminRepository.deleteById(id);
+        return "Admin with id "+id+" has been deleted successfully.";
+    }
 
 }
--- BackEnd/BackEnd/src/main/java/com/example/BackEnd/exception/AdminNotFoundException.java
+++ BackEnd/BackEnd/src/main/java/com/example/BackEnd/exception/AdminNotFoundException.java
@@ -1,4 +1,7 @@
 package com.example.BackEnd.exception;
 
-public class AdminNotFoundException {
+public class AdminNotFoundException extends RuntimeException{
+    public AdminNotFoundException(int id){
+        super("Could not found the user with id "+ id);
+    }
 }
