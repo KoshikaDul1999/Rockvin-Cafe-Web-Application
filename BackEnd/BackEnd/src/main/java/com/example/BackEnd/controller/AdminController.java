b5a33d312b89938a799bf94252ee1c58cdae3932
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-03-31 12:35:09 +0530

    create add user

--- BackEnd/BackEnd/src/main/java/com/example/BackEnd/controller/AdminController.java
+++ BackEnd/BackEnd/src/main/java/com/example/BackEnd/controller/AdminController.java
@@ -1,4 +1,24 @@
 package com.example.BackEnd.controller;
 
+import com.example.BackEnd.model.Admin;
+import com.example.BackEnd.repository.AdminRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
 public class AdminController {
+
+    @Autowired
+    private AdminRepository adminRepository;
+
+    @PostMapping("/admin")
+    Admin newAdmin(@RequestBody Admin newAdmin){
+        return adminRepository.save(newAdmin);
+    }
+
+
 }
