0999859619d4d5150d5f2b63b0edd959682063bd
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-04-02 11:28:39 +0530

    create chef frontend

--- FrontEnd/src/Chef/ViewChef.js
+++ FrontEnd/src/Chef/ViewChef.js
@@ -2,50 +2,50 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react'
 import { Link, useParams } from 'react-router-dom'
 
-export default function ViewAdmin() {
+export default function ViewChef() {
 
-    const [admin,setAdmin]=useState({
-        admin_name:"",
-        admin_username:"",
-        admin_email:"",
-        admin_password:"",
+    const [chef,setChef]=useState({
+        chef_name:"",
+        chef_username:"",
+        chef_email:"",
+        chef_password:"",
     });
 
-    const {id}=useParams();
+    const {chef_id}=useParams();
 
     useEffect(()=>{
-        loadAdmin()
+        loadChef()
     },[])
 
-    const loadAdmin = async () => {
-        const result=await axios.get(`http://localhost:8080/admin/${id}`)
-        setAdmin(result.data)
+    const loadChef = async () => {
+        const result=await axios.get(`http://localhost:8080/chef/${chef_id}`)
+        setChef(result.data)
     }
 
   return (
     <div className='container'>
         <div className='row'>
-            <div className='col-md-16 offset-md-3 border rounded p-4 mt-2 shadow p-3 mb-2 bg-dark text-white'>
-                <h2 className='text-center m-4'>Admin Details</h2>
+            <div className='col-md-16 offset-md-3 border rounded p-4 mt-2 shadow p-3 mb-2 bg-info text-white'>
+                <h2 className='text-center m-4'>Chef Details</h2>
                 <div className='card'>
                     <div className='card-header'>
-                        Details of Admin Id : {admin.id}
+                        Details of Chef Id : {chef.id}
                         <ul className='list-group list-group-flush'>
                             <li className='list-group-item'>
-                                <b>Admin Name : </b>
-                                {admin.admin_name}
+                                <b>Chef Name : </b>
+                                {chef.chef_name}
                             </li>
                             <li className='list-group-item'>
-                                <b>Admin UserName : </b>
-                                {admin.admin_username}
+                                <b>Chef UserName : </b>
+                                {chef.chef_username}
                             </li>
                             <li className='list-group-item'>
-                                <b>Admin Email : </b>
-                                {admin.admin_email}
+                                <b>Chef Email : </b>
+                                {chef.chef_email}
                             </li>
                             <li className='list-group-item'>
-                                <b>Admin Password : </b>
-                                {admin.admin_password}
+                                <b>Chef Password : </b>
+                                {chef.chef_password}
                             </li>
                         </ul>
                     </div>
