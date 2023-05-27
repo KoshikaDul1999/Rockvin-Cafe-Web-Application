bb28ffcbb64168bd0980ac523aef538f3b9aadf7
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-05-27 17:52:22 +0530

    updated exceptions and repo

--- BackEnd/src/main/java/com/example/backend/Repository/ChefRepository.java
+++ BackEnd/src/main/java/com/example/backend/Repository/ChefRepository.java
@@ -1,4 +1,7 @@
 package com.example.backend.Repository;
 
-public interface ChefRepository {
+import com.example.backend.Model.Chef;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChefRepository extends JpaRepository<Chef,Integer> {
 }
