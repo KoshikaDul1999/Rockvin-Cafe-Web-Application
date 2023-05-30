53fc70c8be880c0045131bf4155b74bd4c348904
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-05-30 13:34:15 +0530

    Update index.js

--- FrontEnd/src/Login/index.js
+++ FrontEnd/src/Login/index.js
@@ -35,7 +35,7 @@ export default function SignIn() {
     };
 
     // Send a POST request to your Spring Boot endpoint
-    axios.post("http://localhost:8080/api/auth/authenticate", formData)
+    axios.post("http://localhost:8080/login", formData)
       .then((response) => {
         // Handle the response from the server
         if (response.status === 200){
