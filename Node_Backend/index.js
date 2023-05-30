746969c81e04811cf985932619cc5193fa967c70
Sanjeewa Chathuranga <sanjeewachaturanga0@gmail.com>
2023-05-31 02:36:02 +0530

    Foods Controllers
    Co-Authored-By: Hansini98-R <104292861+Hansini98-R@users.noreply.github.com>
    Co-Authored-By: Lakshi vihanga <103477094+RPLVihanga@users.noreply.github.com>

--- Node_Backend/index.js
+++ Node_Backend/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import router from "./routes/UserRouter.js";
+import router from "./routes/FoodRoutes.js";
 import { fileURLToPath } from "url";
 import path from "path";
 
--- Node_Backend/models/UserModel.js
+++ Node_Backend/models/UserModel.js
@@ -1,49 +1,49 @@
-import {Sequelize} from "sequelize";
-import db from "../config/Database.js";
+// import {Sequelize} from "sequelize";
+// import db from "../config/Database.js";
  
-const {DataTypes} = Sequelize;
+// const {DataTypes} = Sequelize;
  
-const User = db.define('users',{
-    username: {
-        type:DataTypes.STRING,
-        allowNull:false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull:false,
-    },
-    tel: {
-        type: DataTypes.STRING,
-        unique: true,
-        allowNull:false,
-    },
-    password: {
-        type:DataTypes.STRING,
-        allowNull: false,
-    },
-    role: {
-        type:DataTypes.INTEGER,
-        allowNull:false,
-    },
-    profile:{
-        type:DataTypes.STRING,
-        allowNull:true,
-    },
-    email_verified: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-    },
-    tel_verified: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-    },
-},{
-    freezeTableName:true
-});
+// const User = db.define('users',{
+//     username: {
+//         type:DataTypes.STRING,
+//         allowNull:false,
+//     },
+//     email: {
+//         type: DataTypes.STRING,
+//         unique: true,
+//         allowNull:false,
+//     },
+//     tel: {
+//         type: DataTypes.STRING,
+//         unique: true,
+//         allowNull:false,
+//     },
+//     password: {
+//         type:DataTypes.STRING,
+//         allowNull: false,
+//     },
+//     role: {
+//         type:DataTypes.INTEGER,
+//         allowNull:false,
+//     },
+//     profile:{
+//         type:DataTypes.STRING,
+//         allowNull:true,
+//     },
+//     email_verified: {
+//         type: DataTypes.INTEGER,
+//         allowNull: true,
+//     },
+//     tel_verified: {
+//         type: DataTypes.INTEGER,
+//         allowNull: true,
+//     },
+// },{
+//     freezeTableName:true
+// });
  
-export default User;
+// export default User;
  
-(async()=>{
-    await db.sync();
-})();
+// (async()=>{
+//     await db.sync();
+// })();
