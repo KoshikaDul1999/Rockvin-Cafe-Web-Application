32a9bacee818fd0075667e3b3373bf7e6b237883
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-05-31 13:46:42 +0530

    updated
    Co-Authored-By: Hansini98-R <104292861+Hansini98-R@users.noreply.github.com>
    Co-Authored-By: Lakshi vihanga <103477094+RPLVihanga@users.noreply.github.com>
    Co-Authored-By: Sanjeewa Chathuranga <61737806+sanjeewa999@users.noreply.github.com>

--- Node_Backend/controllers/AdminController.js
+++ Node_Backend/controllers/AdminController.js
@@ -1,58 +1,58 @@
-import Admins from "../models/AdminModel.js";
+// import Admins from "../models/AdminModel.js";
  
-export const getAdmins = async(req, res) =>{
-    try {
-        const response = await Foods.findAll();
-        res.status(200).json(response);
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+// export const getAdmins = async(req, res) =>{
+//     try {
+//         const response = await Foods.findAll();
+//         res.status(200).json(response);
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// }
  
-export const getAdminById = async(req, res) =>{
-    try {
-        const response = await Admins.findOne({
-            where:{
-                email: req.params.id
-            }
-        });
-        res.status(200).json(response);
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+// export const getAdminById = async(req, res) =>{
+//     try {
+//         const response = await Admins.findOne({
+//             where:{
+//                 email: req.params.id
+//             }
+//         });
+//         res.status(200).json(response);
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// }
  
-export const createAdmin = async(req, res) =>{
-    try {
-        await Admins.create(req.body);
-        res.status(201).json({msg: "Admin Created"});
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+// export const createAdmin = async(req, res) =>{
+//     try {
+//         await Admins.create(req.body);
+//         res.status(201).json({msg: "Admin Created"});
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// }
  
-export const updateAdmin = async(req, res) =>{
-    try {
-        await Admins.update(req.body,{
-            where:{
-                id: req.params.id
-            }
-        });
-        res.status(200).json({msg: "Admin Updated"});
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+// export const updateAdmin = async(req, res) =>{
+//     try {
+//         await Admins.update(req.body,{
+//             where:{
+//                 id: req.params.id
+//             }
+//         });
+//         res.status(200).json({msg: "Admin Updated"});
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// }
  
-export const deleteAdmin = async(req, res) =>{
-    try {
-        await Admins.destroy({
-            where:{
-                id: req.params.id
-            }
-        });
-        res.status(200).json({msg: "Admin Deleted"});
-    } catch (error) {
-        console.log(error.message);
-    }
-}
+// export const deleteAdmin = async(req, res) =>{
+//     try {
+//         await Admins.destroy({
+//             where:{
+//                 id: req.params.id
+//             }
+//         });
+//         res.status(200).json({msg: "Admin Deleted"});
+//     } catch (error) {
+//         console.log(error.message);
+//     }
+// }
