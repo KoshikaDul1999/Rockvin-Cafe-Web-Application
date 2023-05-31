69382fac5cfc7890ba21434507fee0d56bc085ea
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-05-31 11:23:02 +0530

    Update Database.js

--- Node_Backend/config/Database.js
+++ Node_Backend/config/Database.js
@@ -1,14 +1,14 @@
 import {Sequelize} from "sequelize";
 
-// const db = new Sequelize('u812963415_caferockvin','u812963415_caferockvin','S4iko>BTA*',{
-//     host: '191.96.56.1',
-//     dialect: 'mysql'
-// });
-
-const db = new Sequelize('u812963415_caferockvin','root','',{
-    host: 'localhost',
+const db = new Sequelize('u812963415_caferockvin','u812963415_caferockvin','S4iko>BTA*',{
+    host: '191.96.56.1',
     dialect: 'mysql'
 });
+
+// const db = new Sequelize('u812963415_caferockvin','root','',{
+//     host: 'localhost',
+//     dialect: 'mysql'
+// });
  
 
  
