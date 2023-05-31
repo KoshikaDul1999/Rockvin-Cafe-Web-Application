06a812611dfc46a90759aeb888878f25a05e2bc2
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-05-31 13:43:30 +0530

    updated
    Co-Authored-By: Hansini98-R <104292861+Hansini98-R@users.noreply.github.com>
    Co-Authored-By: Lakshi vihanga <103477094+RPLVihanga@users.noreply.github.com>
    Co-Authored-By: Sanjeewa Chathuranga <61737806+sanjeewa999@users.noreply.github.com>

---
+++ Node_Backend/models/AdminModel.js
@@ -0,0 +1,31 @@
+import {Sequelize} from "sequelize";
+import db from "../config/Database.js";
+ 
+const {DataTypes} = Sequelize;
+ 
+const Admins = db.define('admin',{
+    id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey:true
+    },
+    
+    email: {
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+    password: {
+        type:DataTypes.STRING,
+        allowNull:false,
+    },
+
+
+},{
+    freezeTableName:true
+});
+ 
+export default Admins;
+ 
+(async()=>{
+    await db.sync();
+})();
--- Node_Backend/routes/MainRouter.js
+++ Node_Backend/routes/MainRouter.js
@@ -2,6 +2,7 @@ import express from "express";
 import { getCategories, getCategoryById, createCategory, updateCategory, deleteCategory} from "../controllers/CategoryController.js";
 import { getFoods, getFoodById, updateFood, createFood, deleteFood } from "../controllers/FoodController.js";
 import { deleteOrderDetails, updateOrderDetails, getOrderDetailsById, getOrderDetails, createOrderDetails } from "../controllers/OrderDetailsController.js";
+import { getAdmins, getAdminById, createAdmin, updateAdmin, deleteAdmin} from "../controllers/CategoryController.js";
 
 const router = express.Router();
 
@@ -22,5 +23,11 @@ router.get('/OrderDetails/:id', getOrderDetailsById);
 router.post('/OrderDetails', createOrderDetails);
 router.patch('/OrderDetails/:id', updateOrderDetails);
 router.delete('/OrderDetails/:id', deleteOrderDetails);
+
+router.get('/admins', getAdmins);
+router.get('/admin/:id', getAdminById);
+router.post('/admin', createAdmin);
+router.put('/admins/:id', updateAdmin);
+router.delete('/admin/:id', deleteAdmin);
 
 export default router;
