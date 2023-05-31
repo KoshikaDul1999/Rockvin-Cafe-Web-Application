1f213e9ab1f36dd7f1ac14a57284cb8bb7105c41
Sanjeewa Chathuranga <sanjeewachaturanga0@gmail.com>
2023-05-31 11:22:08 +0530

    Update

---
+++ Node_Backend/routes/OrderDetailsRouter.js
@@ -0,0 +1,14 @@
+// import express from "express";
+
+// import { deleteOrderDetails, updateOrderDetails, getOrderDetailsById, getOrderDetails, createOrderDetails } from "../controllers/OrderDetailsController.js";
+
+
+// const router = express.Router();
+ 
+// router.get('/OrderDetails', getOrderDetails);
+// router.get('/OrderDetails/:id', getOrderDetailsById);
+// router.post('/OrderDetails', createOrderDetails);
+// router.patch('/OrderDetails/:id', updateOrderDetails);
+// router.delete('/OrderDetails/:id', deleteOrderDetails);
+
+// export default router;
