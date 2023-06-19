1fc47f9600192eb6e618f2bf17776d5eec01452f
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-06-19 13:23:11 +0530

    updated backend
    Co-Authored-By: Hansini98-R <104292861+Hansini98-R@users.noreply.github.com>

--- Node_Backend/controllers/FoodController.js
+++ Node_Backend/controllers/FoodController.js
@@ -1,58 +1,58 @@
-// import Foods from "../models/FoodModel.js";
+import Foods from "../models/FoodModel.js";
  
-// export const getFoods = async(req, res) =>{
-//     try {
-//         const response = await Foods.findAll();
-//         res.status(200).json(response);
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-// }
+export const getFoods = async(req, res) =>{
+    try {
+        const response = await Foods.findAll();
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
  
-// export const getFoodById = async(req, res) =>{
-//     try {
-//         const response = await Foods.findOne({
-//             where:{
-//                 id: req.params.id
-//             }
-//         });
-//         res.status(200).json(response);
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-// }
+export const getFoodById = async(req, res) =>{
+    try {
+        const response = await Foods.findOne({
+            where:{
+                id: req.params.id
+            }
+        });
+        res.status(200).json(response);
+    } catch (error) {
+        console.log(error.message);
+    }
+}
  
-// export const createFood = async(req, res) =>{
-//     try {
-//         await Foods.create(req.body);
-//         res.status(201).json({msg: "Food Created"});
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-// }
+export const createFood = async(req, res) =>{
+    try {
+        await Foods.create(req.body);
+        res.status(201).json({msg: "Food Created"});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
  
-// export const updateFood = async(req, res) =>{
-//     try {
-//         await Foods.update(req.body,{
-//             where:{
-//                 id: req.params.id
-//             }
-//         });
-//         res.status(200).json({msg: "Food Updated"});
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-// }
+export const updateFood = async(req, res) =>{
+    try {
+        await Foods.update(req.body,{
+            where:{
+                id: req.params.id
+            }
+        });
+        res.status(200).json({msg: "Food Updated"});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
  
-// export const deleteFood = async(req, res) =>{
-//     try {
-//         await Foods.destroy({
-//             where:{
-//                 id: req.params.id
-//             }
-//         });
-//         res.status(200).json({msg: "Food Deleted"});
-//     } catch (error) {
-//         console.log(error.message);
-//     }
-// }
+export const deleteFood = async(req, res) =>{
+    try {
+        await Foods.destroy({
+            where:{
+                id: req.params.id
+            }
+        });
+        res.status(200).json({msg: "Food Deleted"});
+    } catch (error) {
+        console.log(error.message);
+    }
+}
