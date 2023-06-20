fae0a73b84d047bb5ae419f2fed254a5f2195c64
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-06-20 23:55:41 +0530

    updated

--- Node_Backend/controllers/FoodController.js
+++ Node_Backend/controllers/FoodController.js
@@ -1,5 +1,5 @@
 import Foods from "../models/FoodModel.js";
- 
+
 export const getFoods = async(req, res) =>{
     try {
         const response = await Foods.findAll();
@@ -56,3 +56,51 @@ export const deleteFood = async(req, res) =>{
         console.log(error.message);
     }
 }
+
+// const Food = require('./models/food');
+// const Category = require('./models/category');
+// const multer = require('multer');
+
+// // Set up multer for handling file uploads
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, 'uploads/'); // Specify the destination folder for storing uploaded files
+//   },
+//   filename: (req, file, cb) => {
+//     cb(null, Date.now() + '_' + file.originalname); // Generate a unique filename for the uploaded file
+//   },
+// });
+
+// const upload = multer({ storage });
+
+// // Endpoint for uploading a product with an image
+// app.post('/upload', upload.single('food_image'), async (req, res) => {
+//   try {
+//     // Retrieve the form data from the request body
+//     const { food_id, food_name, food_price, food_desc, food_category } = req.body;
+
+//     // Get the uploaded image filename from the request file
+//     const food_image = req.file.filename;
+
+//     // Find the category by its name in the Category table
+//     const category = await Category.findOne({ name: food_category });
+
+//     // Create a new Food document with the form data
+//     const food = new Food({
+//       food_id,
+//       food_name,
+//       food_price,
+//       food_image,
+//       food_desc,
+//       food_category: category._id, // Assign the category's ID to the food_category field
+//     });
+
+//     // Save the food document to the database
+//     await food.save();
+
+//     res.status(201).json({ msg: 'Food Created' });
+//   } catch (error) {
+//     console.log(error.message);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
+// });
