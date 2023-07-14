c9bd64292f6a19065dbbb27c06e1dfd1d1ce2e29
Koshika Dulanjana <87201309+KoshikaDul1999@users.noreply.github.com>
2023-07-14 13:05:34 +0530

    updated delete popup alert box

--- FrontEnd/src/Pages/Category/index.js
+++ FrontEnd/src/Pages/Category/index.js
@@ -1,5 +1,3 @@
-
-
 import { Typography } from "antd";
 import { Link, useParams } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
@@ -9,80 +7,193 @@ import PageContent from '../../Components/PageContent';
 
 function Category() {
 
-    const [categories,setCategories]=useState([]);
+  const [categories,setCategories]=useState([]);
 
-    const {category_id}=useParams()
+  const {category_id}=useParams()
 
-    useEffect(() => {
-        loadCategories();
-    },[]);
+  useEffect(() => {
+    loadCategories();
+  },[]);
 
-    const loadCategories = async () => {
-        const result = await axios.get("http://localhost:5000/categories");
-        setCategories(result.data);
-    };
+  const loadCategories = async () => {
+    const result = await axios.get("http://localhost:5000/categories");
+    setCategories(result.data);
+  };
+
+//   const deleteCategory = async (category_id) => {
+//     const result = await axios.delete(`http://localhost:5000/category/${category_id}`);
+//     loadCategories();
+
+//     // Show a confirmation popup
+//     const confirmDelete = window.confirm("Are you sure you want to delete this category?");
+//     if (!confirmDelete) {
+//       // Do not delete the category
+//       console.log("Category was not deleted.");
+//     } else {
+//       // Delete the category
+//       const result = await axios.delete(`http://localhost:5000/category/${category_id}`);
+//       loadCategories();
+//     }
+//   };
 
     const deleteCategory = async (category_id) => {
-        const result = await axios.delete(`http://localhost:5000/category/${category_id}`)
-        loadCategories()
-    }
+        // Show a confirmation popup
+        const confirmDelete = window.confirm("Are you sure you want to delete this category?");
+        if (confirmDelete) {
+        // Delete the category
+        await axios.delete(`http://localhost:5000/category/${category_id}`);
+        loadCategories();
+        } else {
+        // Do not delete the category
+        console.log("Category was not deleted.");
+        }
+    };
+  
 
-    return (
-        <div className="SideMenuAndPageContent">
-        <SideMenu></SideMenu>
-        <PageContent></PageContent>
-            <div>
-                <Typography.Title level={4}>Our Category</Typography.Title>
-                <Link className='btn btn-primary' to="/addnewcategory">Add New category</Link>
-                <div className="container">
-                    <div className="py-4">
-                        <table className="table border shadow-inner, table-primary">
-                        <thead className="thead-dark">
-                            <tr>
-                                <th scope="col">ID</th>
-                                <th scope="col">Category Name</th>
-                                <th scope="col">Category Description</th>
-                                <th scope="col">Action</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                        {
-                            categories.map((category,index)=>(
-                                <tr>
-                                    <th scope="row" key={index}>{index+1}</th>
-                                    <td>{category.cate_name}</td>
-                                    <td>{category.cate_desc}</td>
-                                    <td>
-                                        <Link className='btn btn-primary mx-2'
-                                            to={`/viewcategory/${category.cate_id}`}
-                                        >
-                                            View
-                                        </Link>
+  return (
+    <div className="SideMenuAndPageContent">
+      <SideMenu></SideMenu>
+      <PageContent></PageContent>
+      <div>
+        <Typography.Title level={4}>Our Category</Typography.Title>
+        <Link className='btn btn-primary' to="/addnewcategory">Add New category</Link>
+        <div className="container">
+          <div className="py-4">
+            <table className="table border shadow-inner, table-primary">
+              <thead className="thead-dark">
+                <tr>
+                  <th scope="col">ID</th>
+                  <th scope="col">Category Name</th>
+                  <th scope="col">Category Description</th>
+                  <th scope="col">Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                {categories.map((category, index) => (
+                  <tr key={index}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{category.cate_name}</td>
+                    <td>{category.cate_desc}</td>
+                    <td>
+                      <Link
+                        className='btn btn-primary mx-2'
+                        to={`/viewcategory/${category.cate_id}`}
+                      >
+                        View
+                      </Link>
 
-                                        <Link className='btn btn-outline-primary mx-2'
-                                        to={`/editcategory/${category.cate_id}`}
-                                        >
-                                            Edit
-                                        </Link>
+                      <Link
+                        className='btn btn-outline-primary mx-2'
+                        to={`/editcategory/${category.cate_id}`}
+                      >
+                        Edit
+                      </Link>
 
-                                        <button className='btn btn-danger mx-2'
-                                            onClick={() => deleteCategory(category.cate_id)}
-                                        >
-                                            Delete
-                                        </button>
-                                    </td>
-                                </tr> 
-                            ))
-                        }
-                        </tbody>
-                        </table>
-                    </div>
-                </div>
-            </div>
+                      <button
+                        className='btn btn-danger mx-2'
+                        onClick={() => deleteCategory(category.cate_id)}
+                      >
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
-    ); 
+      </div>
+    </div>
+  );
 }
-export default Category
+
+export default Category;
+
+
+
+// import { Typography } from "antd";
+// import { Link, useParams } from 'react-router-dom';
+// import React, { useEffect, useState } from 'react';
+// import axios from 'axios';
+// import SideMenu from '../../Components/SideMenu';
+// import PageContent from '../../Components/PageContent';
+
+// function Category() {
+
+//     const [categories,setCategories]=useState([]);
+
+//     const {category_id}=useParams()
+
+//     useEffect(() => {
+//         loadCategories();
+//     },[]);
+
+//     const loadCategories = async () => {
+//         const result = await axios.get("http://localhost:5000/categories");
+//         setCategories(result.data);
+//     };
+
+//     const deleteCategory = async (category_id) => {
+//         const result = await axios.delete(`http://localhost:5000/category/${category_id}`)
+//         loadCategories()
+//     }
+
+//     return (
+//         <div className="SideMenuAndPageContent">
+//         <SideMenu></SideMenu>
+//         <PageContent></PageContent>
+//             <div>
+//                 <Typography.Title level={4}>Our Category</Typography.Title>
+//                 <Link className='btn btn-primary' to="/addnewcategory">Add New category</Link>
+//                 <div className="container">
+//                     <div className="py-4">
+//                         <table className="table border shadow-inner, table-primary">
+//                         <thead className="thead-dark">
+//                             <tr>
+//                                 <th scope="col">ID</th>
+//                                 <th scope="col">Category Name</th>
+//                                 <th scope="col">Category Description</th>
+//                                 <th scope="col">Action</th>
+//                             </tr>
+//                         </thead>
+//                         <tbody>
+//                         {
+//                             categories.map((category,index)=>(
+//                                 <tr>
+//                                     <th scope="row" key={index}>{index+1}</th>
+//                                     <td>{category.cate_name}</td>
+//                                     <td>{category.cate_desc}</td>
+//                                     <td>
+//                                         <Link className='btn btn-primary mx-2'
+//                                             to={`/viewcategory/${category.cate_id}`}
+//                                         >
+//                                             View
+//                                         </Link>
+
+//                                         <Link className='btn btn-outline-primary mx-2'
+//                                         to={`/editcategory/${category.cate_id}`}
+//                                         >
+//                                             Edit
+//                                         </Link>
+
+//                                         <button className='btn btn-danger mx-2'
+//                                             onClick={() => deleteCategory(category.cate_id)}
+//                                         >
+//                                             Delete
+//                                         </button>
+//                                     </td>
+//                                 </tr> 
+//                             ))
+//                         }
+//                         </tbody>
+//                         </table>
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//     ); 
+// }
+// export default Category
 
 
 // import { Typography } from 'antd';
